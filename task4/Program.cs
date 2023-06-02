66a2b3eab94af2fdda3e8353227bd000163c8382
darina1009 <45716384+darina1009@users.noreply.github.com>
2023-06-02 18:15:55 +0300

    Add files via upload

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void Task4(){
+    Console.WriteLine("Введите число");
+    int num6 = Convert.ToInt32(Console.ReadLine());
+
+    if (num6%2 == 0){
+Console.WriteLine("ДА");
+    }
+    else {
+        Console.WriteLine("НЕТ");
+    }
+    
+}
+
+Task4();
