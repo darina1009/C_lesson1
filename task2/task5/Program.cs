c14de1bd5769cb3200c2fd1619be80df72407207
darina1009 <45716384+darina1009@users.noreply.github.com>
2023-06-02 18:15:01 +0300

    Add files via upload

---
+++ task2/task5/Program.cs
@@ -0,0 +1,23 @@
+﻿
+void Task5(){
+    Console.WriteLine("Введите число");
+    int num7 = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 1; i <= num7; i++ ){
+        if (i%2 == 0){
+            Console.WriteLine(i);
+        }
+    }
+}
+
+
+//вариант для умных
+
+void Task6(){
+    Console.WriteLine("Введите число");
+    int num7 = Convert.ToInt32(Console.ReadLine());
+
+   Enumerable.Range(1,num7).Where(x => x%2 == 0).ToList().ForEach(x => Console.WriteLine(x));
+}
+
+Task5();
---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+void Task3(){
+        Console.WriteLine("Введите первое число");
+    int num3 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int num4 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите третье число");
+    int num5 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(new int[] {num3,num4,num5}.Max()); //создаем масив и ввыводим максимальное число
+}
+
+Task3();
